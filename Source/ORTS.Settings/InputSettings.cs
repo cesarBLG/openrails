1a4bc7a9661e44b39cee3d4df20d9558e0c334fa
Csantucci <carlosanit1@gmail.com>
2021-11-12 15:29:32 +0100

    Distributed Power: rearrange key assignments

--- Source/ORTS.Settings/InputSettings.cs
+++ Source/ORTS.Settings/InputSettings.cs
@@ -483,12 +483,13 @@ namespace ORTS.Settings
             Commands[(int)UserCommand.ControlSpeedRegulatorSelectedSpeedToZero] = new UserCommandKeyInput(0x1E, KeyModifiers.Shift | KeyModifiers.Alt);
             // Distributed power
             Commands[(int)UserCommand.ControlDPMoveToFront] = new UserCommandKeyInput(0x18, KeyModifiers.Control); //O
-            Commands[(int)UserCommand.ControlDPMoveToBack] = new UserCommandKeyInput(0x17, KeyModifiers.Control); //I
+            Commands[(int)UserCommand.ControlDPMoveToBack] = new UserCommandKeyInput(0x18, KeyModifiers.Control | KeyModifiers.Shift); //O
             Commands[(int)UserCommand.ControlDPTraction] = new UserCommandKeyInput(0x26, KeyModifiers.Control); //L
-            Commands[(int)UserCommand.ControlDPIdle] = new UserCommandKeyInput(0x25, KeyModifiers.Control); //K
-            Commands[(int)UserCommand.ControlDPBrake] = new UserCommandKeyInput(0x24, KeyModifiers.Control); //J
+            Commands[(int)UserCommand.ControlDPIdle] = new UserCommandKeyInput(0x26, KeyModifiers.Control | KeyModifiers.Shift); //L
+            Commands[(int)UserCommand.ControlDPBrake] = new UserCommandKeyInput(0x28, KeyModifiers.Control); //
+            Commands[(int)UserCommand.ControlDPIdle] = new UserCommandKeyInput(0x28, KeyModifiers.Control | KeyModifiers.Shift); //
             Commands[(int)UserCommand.ControlDPMore] = new UserCommandKeyInput(0x16, KeyModifiers.Control); //U
-            Commands[(int)UserCommand.ControlDPLess] = new UserCommandKeyInput(0x31, KeyModifiers.Control); //N
+            Commands[(int)UserCommand.ControlDPLess] = new UserCommandKeyInput(0x16, KeyModifiers.Control | KeyModifiers.Shift); //U
 
             Commands[(int)UserCommand.DebugClockBackwards] = new UserCommandKeyInput(0x0C);
             Commands[(int)UserCommand.DebugClockForwards] = new UserCommandKeyInput(0x0D);
